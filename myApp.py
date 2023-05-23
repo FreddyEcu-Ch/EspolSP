444b7b492d9a9a793528b627c8878b73b5797934
fredd <freddypcarrion@gmail.com>
2023-05-23 10:14:53 -0500

    First commit.

---
+++ myApp.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Agregar título de la aplicación web
+st.title("Welcome to Software for PE")
+
+# Agregar información
+st.markdown("---")
+st.write("Esta materia se encargará de enseñarnos a diseñar software/app para PE.")
