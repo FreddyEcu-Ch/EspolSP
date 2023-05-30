26b856679c836c35f67696389e7ca0abf589b567
fredd <freddypcarrion@gmail.com>
2023-05-30 10:55:21 -0500

    poes function

--- poes/Model/poes.py
+++ poes/Model/poes.py
@@ -1,1 +1,20 @@
-5 + 5
+# Create POES function
+
+def poes(area, h, poro, swi, boi):
+    """
+
+    Parameters
+    ----------
+    area: Drainage area (acres)
+    h: Thickness
+    poro: Porosity (fraction)
+    swi: Water saturation (fraction)
+    boi: Oil volume factor (rb/stb)
+
+    Returns
+    -------
+    poes: bbl
+    """
+
+    Poes = (7758 * area * h * poro * (1 - swi)) / boi
+    return Poes
