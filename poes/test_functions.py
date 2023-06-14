74d0bc31b4437d47f1bc352de2ed5a3eec30ef07
jodavila98 <ajdavila@espol.edu.ec>
2023-06-14 09:36:34 -0500

    Se intentó

--- poes/test_functions.py
+++ poes/test_functions.py
@@ -1,20 +1,25 @@
 #%% Import Python Libraries
 
-
-
+import xlwings as xw
+import numpy as np
+import pandas as pd
+from scipy.stats import expon, lognorm, norm, triang, uniform
+from poes.Model.poes import poes
+from poes.Model.utils import param_stoiip
 
 
 
 
 
 #%% #%% Call workbook
-
-
-
+wb = xw.Book("poes/Controller/poes.xlsm")
+sheet= wb.sheets["Summary"]
 #%% Call Dataframe
 
+    params = sheet[DET_VALUES].options(np.array, transpose=True).value
+    sheet[POES_DET].value = poes(*params)
 
-
+    Distributions = [Triangular, Normal, Lognormal, Normal, Exponential]
 
 
 
